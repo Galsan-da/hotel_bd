4732802585361994387b93173c50b038f0ab5c4f
Galsan Zhanchipov <galsan86@mail.ru>
2024-12-14 17:20:43 +0300

    # Changes to be committed: #	new file:   __pycache__/main.cpython-311.pyc #	modified:   main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 
+from fastapi import FastAPI, Query, Body
+import uvicorn
+
+app = FastAPI() #создаем экземпляр приложения
+
+hotels = [
+    {'id': 1, 'title': 'Sochi', 'name': 'sochi', },
+    {'id': 2, 'title': 'Moscow', 'name': 'moscow', },
+    {'id': 3, 'title': 'New York', 'name': 'New York', },
+]
+@app.get("/hotels")
+def get_hotel(
+        id: int | None = Query(default=None, description="ID города"),
+        title: str | None = Query(default=None, description="Название города")
+):
+    ct = [city for city in data_db if id == city['id'] or title == city['title']]
+    return ct
+@app.delete("/hotels/{hotels_id}")
+def delete_hotel(hotels_id: int):
+    global data_db
+    data_db = [hotel for hotel in data_db if hotels_id != hotel['id']]
+    return {'status': 'ok'}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
