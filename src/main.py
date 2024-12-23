0b95afbfa78a04c798577144056d9c2c22013b92
Galsan Zhanchipov <galsan86@mail.ru>
2024-12-23 17:43:00 +0300

    # Changes to be committed: #	modified:   requirements.txt #	modified:   src/__pycache__/main.cpython-311.pyc #	modified:   src/database.py #	modified:   src/main.py #	new file:   src/models/hotels.py

--- src/main.py
+++ src/main.py
@@ -7,8 +7,6 @@ from fastapi import FastAPI
 sys.path.append(str(Path(__file__).parent.parent))
 
 from src.api.hotels import router as router_hotels
-from src.config import settings
-print(f"{settings.DB_URL=}")
 
 # Создаем экземпляр FastAPI приложения
 app = FastAPI()
