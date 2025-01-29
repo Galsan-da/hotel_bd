0b3e8b179c1abb903a9b9eecbfbba3b07cd043a4
Galsan Zhanchipov <galsan86@mail.ru>
2025-01-29 21:17:48 +0300

    # Changes to be committed: #	modified:   src/__pycache__/main.cpython-311.pyc #	modified:   src/main.py #	modified:   src/schemas/users.py

--- src/main.py
+++ src/main.py
@@ -7,14 +7,16 @@ from fastapi import FastAPI
 sys.path.append(str(Path(__file__).parent.parent))
 
 from src.api.hotels import router as router_hotels
+from src.api.auth import router as router_auth
 
 # Создаем экземпляр FastAPI приложения
-app = FastAPI()
+app = FastAPI(docs_url=None)
 
+app.include_router(router_auth)
 app.include_router(router_hotels)
 
-#@app.get("/docs", include_in_schema=False)
-#async def custom_swagger_ui_html(): ...
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():...
 
 # Точка входа для запуска приложения
 if __name__ == "__main__":
