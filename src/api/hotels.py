1a0c81a5cdfbaf10f8717c7b31787e2a568f3c43
Galsan Zhanchipov <galsan86@mail.ru>
2025-05-01 21:24:56 +0300

    # Changes to be committed: #	modified:   src/api/hotels.py

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,12 +1,8 @@
 from fastapi import Query, HTTPException, APIRouter, Body
-from sqlalchemy import insert, select, func
-from typing import Optional
+from sqlalchemy.exc import IntegrityError
 
 from src.schemas.hotels import HotelAdd, HotelPATCH
 from src.api.dependencies import PaginationDep, DBDep
-from src.models.hotels import HotelsOrm
-from src.database import async_session_maker, engine
-from src.repositories.hotels import HotelsRepository
 
 
 # Создаем экземпляр FastAPI приложения
@@ -37,13 +33,16 @@ async def delete_hotel(
     Удалить отель.
 
     """
-    async with async_session_maker() as session:
-      await HotelsRepository(session).delete(id=hotel_id)
-      await session.commit()
+    deleted = await db.hotels.delete(id=hotel_id)
+    if not deleted:
+      raise HTTPException(status_code=404, detail="Отель не найден")
+    await db.commit()
     return {'status': 'ok'}
 
 @router.post("")
-async def create_hotel(hotel_data: HotelAdd = Body(openapi_examples={
+async def create_hotel(
+   db: DBDep,
+   hotel_data: HotelAdd = Body(openapi_examples={
     "1": {"summary": "Сочи", "value":{
         "title": "Отел у моря",
         "location": "Сочи, ул. У Моря, 1",
@@ -51,19 +50,23 @@ async def create_hotel(hotel_data: HotelAdd = Body(openapi_examples={
     "2": {"summary": "Дубай", "value":{
         "title": "Отель Дубай",
         "location": "Дубай, пр. Шейха, 1"
-    }}
-})):
+    }}})
+):
     """
     Создать новый отель.
     """
-    async with async_session_maker() as session:
-      hotel = await HotelsRepository(session).add(hotel_data)
-      await session.commit()
-      return {'status': 'ok', "data": hotel}
+    try:
+        hotel = await db.hotels.add(hotel_data)
+        await db.commit()
+        return {'status': 'ok', "data": hotel}
+    except IntegrityError:
+        await db.session.rollback()
+        raise HTTPException(status_code=400, detail="Отель с такими данными уже существует")
 
 @router.put("/{hotel_id}")
 async def create_update(
     hotel_id: int,
+    db: DBDep,
     hotel_data: HotelAdd= Body(openapi_examples={
     "1": {"summary": "Сочи", "value":{
         "title": "Отел у моря",
@@ -79,30 +82,38 @@ async def create_update(
     Полное обновление отеля.
 
     """
-    async with async_session_maker() as session:
-      await HotelsRepository(session).edit(hotel_data, id=hotel_id)
-      await session.commit()
-      return {'status': 'ok'}
+    try:
+        hotel = await db.hotels.edit(hotel_data, id=hotel_id)
+        if not hotel:
+            raise HTTPException(status_code=404, detail="Отель не найден")
+        await db.commit()
+        return {'status': 'ok', "data": hotel}
+    except IntegrityError:
+        await db.session.rollback()
+        raise HTTPException(status_code=400, detail="Ошибка целостности данных")
 
 @router.patch("/{hotel_id}")
 async def create_patch(
     hotel_id: int,
+    db: DBDep,
     hotel_data: HotelPATCH
 ):
     """
     Частичное обновление отеля по ID.
 
     """
-    async with async_session_maker() as session:
-      await HotelsRepository(session).edit(hotel_data, id=hotel_id, exclude_unset=True)
-      await session.commit()
-      return {'status': 'ok'}
-
-    # Если отель не найден, возвращаем ошибку
-    raise HTTPException(status_code=404, detail="Отель не найден")
+    hotel = await db.hotels.edit(hotel_data, id=hotel_id, exclude_unset=True)
+    if not hotel:
+        raise HTTPException(status_code=404, detail="Отель не найден")
+    await db.commit()
+    return {'status': 'ok', "data": hotel}
 
 @router.get("/{hotel_id}")
-async def get_one_hotel(hotel_id: int):
-   async with async_session_maker() as session:
-      hotel = await HotelsRepository(session).get_one_or_none(id=hotel_id)
-      return {'status': 'ok', "data": hotel}
+async def get_one_hotel(
+    hotel_id: int,
+    db: DBDep
+):
+   hotel = await db.hotels.get_one_or_none(id=hotel_id)
+   if not hotel:
+        raise HTTPException(status_code=404, detail="Отель не найден")
+   return {'status': 'ok', "data": hotel}
